2a217b20c27a58ebc34febe88997b0b074246b43
houWenK <107596428+houWenK@users.noreply.github.com>
2023-06-25 19:59:10 +0800

    2022103212侯文科

---
+++ 2022103212侯文科/start.py
@@ -0,0 +1,29 @@
+from task1 import task1
+from task2 import datasamplying
+from task3.task3 import task3_run
+def main():
+    while True:
+        print("说明：")
+        print(" 作业1对应数字1，作业2对应数字2，作业3对应数字3")
+        print(" 输入e时表示退出")
+        print("请按要求输入：")
+        x=input("请输入1或者2或者3：")
+        if x=='1':
+            task1.dataSampling()
+            print("==============================================================")
+        elif x == "2":
+            # 执行平时作业2
+            datasamplying.dataSampling_run()
+            print("==============================================================")
+        elif x== "3":
+            # 执行平时作业3
+            task3_run()
+            print("==============================================================")
+        elif x.lower() == "e":
+            # 退出程序
+            break
+        else:
+            print("无效的选项，请重新输入！")
+
+if __name__ == '__main__':
+    main()
---
+++ 2022103212侯文科/task1/task1.py
@@ -0,0 +1,34 @@
+import random
+
+def randomInt(datarange,num):
+    result=set()
+    for index in range(0,num):
+        it=iter(datarange)
+        item=random.randint(next(it),next(it))
+        result.add(item)
+    return  result
+
+def randomFloat(datarange,num):
+    result=set()
+    for index in range(0,num):
+        it=iter(datarange)
+        item=random.uniform(next(it),next(it))
+        result.add(item)
+    return  result
+
+def randomStr(datarange,strlength):
+    result=set()
+    item=''.join(random.SystemRandom().choice(datarange) for _ in range(strlength))
+    result.add(item)
+    return  result
+
+def  dataSampling(**kwargs):
+    result_1=randomInt([2,100],10)
+    result_2=randomFloat([11.2,15.6],10)
+    result_3=randomStr(['a','z'],10)
+    print("随机整形：")
+    print(result_1)
+    print("随机字符型：")
+    print(result_2)
+    print("随机字符型：")
+    print(result_3)
