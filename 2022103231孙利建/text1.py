3ddd6cd70742c4f41086c82b7a495342f812d148
houWenK <107596428+houWenK@users.noreply.github.com>
2023-06-25 21:38:23 +0800

    2022103231孙利建

---
+++ 2022103231孙利建/text1.py
@@ -0,0 +1,16 @@
+import random
+
+def dataSampling(**kwargs):
+    result = []
+    for data_type, data_num in kwargs.items():
+        if data_type == "int":
+            result.extend(random.sample(range(1, 100), data_num))
+        elif data_type == "float":
+            result.extend([random.uniform(0, 1) for _ in range(data_num)])
+        elif data_type == "str":
+            result.extend([random.choice("abcdefghijklmnopqrstuvwxyz") for _ in range(data_num)])
+    return result
+
+# 调用示例
+data = dataSampling(int=3, float=3, str=3)
+print(data)
