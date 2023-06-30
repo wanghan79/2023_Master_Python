49c6e6dec0c78c3b7da35f6e37f3d6ba3331bdb1
Yibin <1289639459@qq.com>
2023-06-30 15:53:52 +0800

    Delete 2020103274王怡镔 directory

--- 2020103274王怡镔/python_2/task_test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from python_2.task_data_sampling import data_sampling
-
-
-def test():
-    # 测试
-    return data_sampling(data_type='int', column=4, train_set_num=10000, test_set_num=40, data_range=(1, 1000),
-                        label_range=(0, 1))
